b7d0b6b294069a04d19ff49387d6c2c35c70da68
Kono Steve <stevekono77@gmail.com>
2024-09-21 07:34:26 +0100

    installed added daisyui tailwindcss plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,9 @@
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
+import typography from "@tailwindcss/typography";
+import daisyui from "daisyui";
+
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
@@ -10,5 +13,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [typography, daisyui],
 } satisfies Config;
